568fb02ac12d23421f4b65eff6f0a60372903ebc
adib2215 <adiburr28@gmail.com>
2025-12-09 22:49:13 +0600

    carousel and navber updated

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenu = document.getElementById('mobile-menu');
-const menuIcon = document.getElementById('menu-icon');
+document.addEventListener('DOMContentLoaded', function () {
 
-menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-    menuIcon.classList.toggle('fa-bars');
-    menuIcon.classList.toggle('fa-x');
-})
+    var darkIcon = document.getElementById('theme-toggle-dark-icon');
+    var lightIcon = document.getElementById('theme-toggle-light-icon');
+    var btn = document.getElementById('theme-toggle');
 
+    var isDark = (localStorage.getItem('color-theme') === 'dark') ||
+        (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    if (isDark) {
+        document.documentElement.classList.add('dark');
+        lightIcon.classList.remove('hidden');
+        darkIcon.classList.add('hidden');
+    } else {
+        document.documentElement.classList.remove('dark');
+        darkIcon.classList.remove('hidden');
+        lightIcon.classList.add('hidden');
+    }
+
+    btn.addEventListener('click', function () {
+        darkIcon.classList.toggle('hidden');
+        lightIcon.classList.toggle('hidden');
+
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+        } else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+    });
+
+    var mobileBtn = document.getElementById('mobile-menu-button');
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileBtn.addEventListener('click', function () {
+        mobileMenu.classList.toggle('hidden');
+    });
+
+});
